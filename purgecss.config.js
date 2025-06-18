3d282fdf29b8dbb1d55e2758b781cd247a99b1a5
Ryan O'Farrell <rofarrell.92@gmail.com>
2025-06-17 22:12:52 -0400

    Add safelist

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,10 +1,12 @@
 // purgecss.config.js
 module.exports = {
-  content: ["_site /**/*.html", "_site /**/*.js"],
+  content: ["_site/**/*.html", "_site/**/*.js"],
   css: [
     "_site/assets/css/agency.css",
     "_site/assets/css/bootstrap.min.css",
     "_site/assets/css/all.min.css",
   ],
+  // Add this safelist to protect dynamic classes
+  safelist: ["show", "collapse", "collapsing", "navbar-shrink", "active"],
   output: "_site/assets/css/",
 };
